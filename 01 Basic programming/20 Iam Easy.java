07dc806846d526a76b9f6d6d0a37b26440bb9e3f
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-08-30 17:43:29 +0530

    d3

---
+++ 01 Basic programming/20 Iam Easy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+        int t=s.nextInt();               // Reading input from STDIN
+        for(int i=1;i<=10;i++)
+        {
+        System.out.println(i*t);    // Writing output to STDOUT
+
+}
+
+
+
+    }
+}
